2cd6a73615bea126a8754830b64fe98ad0f85e76
Breno Rodrigues <brenocartoon@hotmail.com>
2021-09-21 14:26:10 -0300

    remove password from update

--- src/dtos/student.update.dto.ts
+++ src/dtos/student.update.dto.ts
@@ -1,10 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Expose } from 'class-transformer';
-import { IsNotEmpty, IsUUID } from 'class-validator';
-import { StudentDTO } from './student.dto';
+import { Exclude, Expose, Type } from 'class-transformer';
+import { IsNotEmpty, IsOptional, IsString, IsUUID, MaxLength } from 'class-validator';
+import { UserToRelationDTO } from './user.dto';
 
 
-export class StudentUpdateDTO extends StudentDTO {
+export class StudentUpdateDTO {
 
   @ApiProperty({
     required: true,
@@ -15,4 +15,166 @@ export class StudentUpdateDTO extends StudentDTO {
   @IsUUID()
   @Expose()
   id: string;
+
+  @ApiProperty({
+    required: true,
+    type: 'string',
+    example: 'Daniel'
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(50)
+  @Expose()
+  name: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Salva Junior'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  @Expose()
+  surname: string;
+
+  @ApiProperty({
+    required: true,
+    type: 'string',
+    example: 'danijunior@gmail.com'
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  @Expose()
+  email: string;
+
+  @ApiProperty({
+    required: false,
+  })
+  @Exclude()
+  password: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Rua XV de Novembro'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  @Expose()
+  street: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: '105'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(6)
+  @Expose()
+  number: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Centro'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  @Expose()
+  district: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Indaiatuba'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  @Expose()
+  city: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'SP'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(2)
+  @Expose()
+  uf: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Apartamento 25'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  @Expose()
+  complement: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: '13330000'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(8)
+  @Expose()
+  cep: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: '19994543355'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(11)
+  @Expose()
+  phone: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'https://anyphoto.net.example/dfaoj3fad0jpo23jlasphoto2'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(300)
+  @Expose()
+  photo_url: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Dani'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(40)
+  @Expose()
+  nickname: string;
+
+  @ApiProperty({
+    required: true,
+    type: [UserToRelationDTO],
+    example: [
+      { 'id': '77d04f2c-fde5-42f5-8ca1-7178bb4aca15' },
+      { 'id': 'a4db6f33-5d2e-40fe-8a05-c989a4c38541' }
+    ]
+  })
+  @IsNotEmpty()
+  @Type(() => UserToRelationDTO)
+  @Expose()
+  personals: UserToRelationDTO[];
 }
--- src/dtos/user.update.dto.ts
+++ src/dtos/user.update.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Expose } from 'class-transformer';
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { Exclude, Expose } from 'class-transformer';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, IsUUID, MaxLength } from 'class-validator';
 import { UserDTO } from './user.dto';
 
-export class UserUpdateDTO extends UserDTO {
+export class UserUpdateDTO {
 
   @ApiProperty({
     required: true,
@@ -14,4 +14,174 @@ export class UserUpdateDTO extends UserDTO {
   @IsUUID()
   @Expose()
   id: string;
+
+  @ApiProperty({
+    required: true,
+    type: 'boolean',
+    example: false
+  })
+  @IsNotEmpty()
+  @IsBoolean()
+  @Expose()
+  admin: boolean;
+
+  @ApiProperty({
+    required: true,
+    type: 'string',
+    example: 'Rafaela'
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(50)
+  @Expose()
+  name: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'da Silva'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  @Expose()
+  surname: string;
+
+  @ApiProperty({
+    required: true,
+    type: 'string',
+    example: 'rafa_silva@gmail.com'
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  @Expose()
+  email: string;
+
+  @ApiProperty({
+    required: false,
+  })
+  @Exclude()
+  password: string;
+
+  @ApiProperty({
+    required: true,
+    type: 'string',
+    example: 'CREF123456PSP'
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(13)
+  @Expose()
+  cref: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Avenida Presidente Kennedy'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  @Expose()
+  street: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: '4563'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(6)
+  @Expose()
+  number: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Vila Vitória'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  @Expose()
+  district: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Indaiatuba'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  @Expose()
+  city: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'SP'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(2)
+  @Expose()
+  uf: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Bloco C'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  @Expose()
+  complement: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: '13333123'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(8)
+  @Expose()
+  cep: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: '19998789878'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(11)
+  @Expose()
+  phone: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'https://anyphoto.net.example/dfaoj3fad0jpo23jlasphoto2'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(300)
+  @Expose()
+  photo_url: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Rafinha'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(40)
+  @Expose()
+  nickname: string;
 }
