39e232e9ab3d60b1a9522857db007e8a45a23b9a
Breno Rodrigues <brenocartoon@hotmail.com>
2021-09-25 23:47:41 -0300

    Change hello message and add whois endpoint

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { Public } from 'src/auth/public.decorator';
 import { AppService } from '../services/app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('whois')
+  whois(@Request() req) {
+    return req.user;
   }
 }
