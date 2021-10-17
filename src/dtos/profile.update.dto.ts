29d92fc161ee1bf5fd109e5184584f0e1340ed9e
Breno Rodrigues <brenocartoon@hotmail.com>
2021-10-17 18:29:17 -0300

    Create profile update DTO

---
+++ src/dtos/profile.update.dto.ts
@@ -0,0 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+import { IsBase64, IsNotEmpty, IsOptional, IsString, IsUUID, MaxLength } from 'class-validator';
+
+export class ProfileUpdateDTO {
+
+  @ApiProperty({
+    required: true,
+    type: 'uuid',
+    example: 'd0fa98cb-d225-4ef3-bc32-c39c7be56fb1'
+  })
+  @IsNotEmpty()
+  @IsUUID()
+  @Expose()
+  id: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string'
+  })
+  @IsOptional()
+  @IsBase64()
+  @MaxLength(300)
+  @Expose()
+  photo_url: string;
+
+  @ApiProperty({
+    required: false,
+    type: 'string',
+    example: 'Penelope'
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(40)
+  @Expose()
+  nickname: string;
+}
