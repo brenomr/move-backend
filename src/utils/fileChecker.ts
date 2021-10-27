9f6d3f1fdb4add610d210f18017d8a612e21d0a4
Breno Rodrigues <brenocartoon@hotmail.com>
2021-10-27 19:59:08 -0300

    Add file extension and size checker

--- src/utils/fileChecker.ts
+++ src/utils/fileChecker.ts
@@ -6,4 +6,14 @@ export const photoChecker = (req: any, file: any, callback: any) => {
   return callback(null, true);
 };
 
+export const fileChecker = (req: any, file: any, callback: any) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif|pdf)$/)) {
+    req.fileValidationError = 'only jpg, jpeg, png, gif and pdf are allowed';
+    return callback(null, false);
+  }
+  return callback(null, true);
+};
+
 export const maxPhotoSize = { fileSize: 1024 * 1024 * 1 };
+
+export const maxFileSize = { fileSize: 1024 * 1024 * 3 };
